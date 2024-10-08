8975c65feb9feaa5596edbb68a10698b7b990dce
Brianchristbc <138142173+Brianchristbc@users.noreply.github.com>
2024-10-07 22:49:51 -0400

    Create method to add cards (error to be fixed for removing books)

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ function Book(title, author, length, readStatus) {
   this.getInfo = function () {
     return this.author;
   };
+  this.deleteBook = function () {
+    console.log("t");
+    return;
+  };
 }
 
 function addBookToLibrary() {
@@ -21,12 +25,46 @@ function addBookToLibrary() {
   myLibrary.push(bookToAdd);
 }
 
+// Book.prototype.deleteBook = function () {
+//   this.style.backgroundColor = "none";
+// };
+
 addBookButton.addEventListener("click", () => {
   addBookToLibrary();
   bookshelf.innerHTML = "";
+  let ticker = -1;
   for (let item of myLibrary) {
+    ticker++;
     let book = document.createElement("div");
-    book.innerText = `${item.title}, by ${item.author}, ${item.length}, ${item.readStatus}`;
+    book.classList.add("book");
+    let title = document.createElement("p");
+    title.innerText = `${item.title}`;
+    let author = document.createElement("p");
+    author.innerText = `${item.author}`;
+    let length = document.createElement("p");
+    length.innerText = `${item.length}`;
+    let readStatus = document.createElement("p");
+    readStatus.innerText = `${item.readStatus}`;
+    let deleteButton = document.createElement("button");
+    deleteButton.innerText = "X";
+    let bookPosition = document.createElement("p");
+    bookPosition.innerText = ticker;
+    bookPosition.style.display = "none";
+    book.appendChild(title);
+    book.appendChild(author);
+    book.appendChild(length);
+    book.appendChild(readStatus);
+    book.appendChild(deleteButton);
     bookshelf.appendChild(book);
+    console.log(myLibrary);
+    deleteButton.addEventListener("click", () => {
+      book.style.display = "none";
+      bookshelf.removeChild(book);
+      myLibrary.splice(bookPosition.innerText, 1);
+      console.log(ticker);
+      console.log(bookPosition);
+      console.log(myLibrary);
+    });
+    console.log(book.classList);
   }
 });
